e91ee42d4b9b2933684d1905a9350a81da40c4ef
mzparulina <pauline.revilla@gmail.com>
2022-12-14 13:04:52 -0500

    Worked with Michael

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  // console.log("horizontolJoin:", horizontalJoin);
+  
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+  // console.log("verticalJoin:", verticalJoin);
 
-module.exports = wordSearch
+  for (let l of verticalJoin) {
+    if (l.includes(word))
+      return true;
+  }
+
+  return false;
+};
+
+const transpose = function(matrix) {
+  
+  const array = [];
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+
+      if (!array[col]) {
+        array[col] = [];
+      }
+      array[col].push(matrix[row][col]);
+    }
+  }
+  return array;
+};
+
+
+//@mzparulina
+//@mkmach
+module.exports = wordSearch;
